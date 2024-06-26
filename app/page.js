44d177da0d0668d8daff254e2ccd502a2f0fa24b
swasthik-007 <mohanty.swastik4@gmail.com>
2024-06-26 20:09:54 +0530

    implement clerk

---
+++ app/page.js
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <Button>hi</Button>
+
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
+}
